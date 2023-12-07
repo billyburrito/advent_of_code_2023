b6dc573cabd0cf2ba0d83dc20cce02622e1a4daa
billyburrito <erinbritz@gmail.com>
2023-12-07 23:22:05 +0000

    removed debug

--- day4/day4.py
+++ day4/day4.py
@@ -5,8 +5,6 @@ sum2 = 0
 
 with open('input') as fp:
     for line in fp:
-        #print("==============================")
-        #print("idx: " + str(idx))
 
         data = line.strip().split(':')
         win, num = data[1].split('|')
@@ -23,35 +21,25 @@ with open('input') as fp:
         actual = list(set(arr_num).intersection(arr_win))
         count = len(actual)
         for y in range(tag[idx]):
-            #print(" y is: " + str(y))
             for x in range(count):
-                #print("  x is: " + str(x))
                 z = idx + x + 1
-                #print("  z is: " + str(z))
                 if z in tag:
                     tag[z] = tag[z] + 1
                 else:
                     tag[z] = 1
-                #print("tag: " + str(tag))
-
-        #print("actual: " + str(actual))
-        #print("len: " + str(len(actual)))
 
         value = 0
         if count > 0:
             value = 1
             for x in range(count-1):
                 value = value * 2
-            #print("value: " + str(value))
             sum = sum + value
-            #print("sum: " + str(sum))
             value = 0
 
         # increment
         idx = idx + 1
 
 print("part 1: " + str(sum))
-#print(tag)
 for ent in tag.values():
     sum2 = sum2 + ent
 print("part 2: " + str(sum2))
