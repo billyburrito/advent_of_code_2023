9a78be3730b29641cfdc5053ab3215769f30214b
billyburrito <erinbritz@gmail.com>
2023-12-11 20:40:45 +0000

    Day5 sample completed but needs to be retooled.

--- day3/day3.py
+++ day3/day3.py
@@ -2,7 +2,7 @@ import re
 
 arr_data = []
 
-with open('input') as fp:
+with open('sample4') as fp:
     for line in fp:
         arr_data.append(list(line.strip()))
 
@@ -20,43 +20,45 @@ for idx, row in enumerate(arr_data):
         #print(idy,col)
         
         
-        if re.match('\d', arr_data[idx][idy]):
+        if re.match(r"\d", arr_data[idx][idy]):
+            print("num  pre: " + num)
             num = num + arr_data[idx][idy]
+            print("num post: " + num)
             
             print(arr_data[idx][idy])
             # check for chars
             # NSEW
             if idx - 1 >= min:
-                if not re.match('\d|\.', arr_data[idx-1][idy]):
+                if not re.match(r"\d|\.", arr_data[idx-1][idy]):
                     hit = True
                     print(arr_data[idx-1][idy])
             if idx + 1 <= xmax:
-                if not re.match('\d|\.', arr_data[idx+1][idy]):
+                if not re.match(r"\d|\.", arr_data[idx+1][idy]):
                     hit = True
                     print(arr_data[idx+1][idy])
             if idy - 1 >= min:
-                if not re.match('\d|\.', arr_data[idx][idy-1]):
+                if not re.match(r"\d|\.", arr_data[idx][idy-1]):
                     hit = True
                     print(arr_data[idx][idy-1])
             if idy + 1 <= ymax:
-                if not re.match('\d|\.', arr_data[idx][idy+1]):
+                if not re.match(r"\d|\.", arr_data[idx][idy+1]):
                     hit = True
                     print(arr_data[idx][idy+1])
             # diags
             if idx - 1 >= min and idy - 1 >= min:
-                if not re.match('\d|\.', arr_data[idx-1][idy-1]):
+                if not re.match(r"\d|\.", arr_data[idx-1][idy-1]):
                     hit = True
                     print(arr_data[idx-1][idy-1])
             if idx + 1 <= xmax and idy + 1 <= ymax:
-                if not re.match('\d|\.', arr_data[idx+1][idy+1]):
+                if not re.match(r"\d|\.", arr_data[idx+1][idy+1]):
                     hit = True
                     print(arr_data[idx+1][idy+1])
             if idy - 1 >= min and idx + 1 <= xmax:
-                if not re.match('\d|\.', arr_data[idx+1][idy-1]):
+                if not re.match(r"\d|\.", arr_data[idx+1][idy-1]):
                     hit = True
                     print(arr_data[idx+1][idy-1])
             if idy + 1 <= ymax and idx - 1 >= min:
-                if not re.match('\d|\.', arr_data[idx-1][idy+1]):
+                if not re.match(r"\d|\.", arr_data[idx-1][idy+1]):
                     hit = True
                     print(arr_data[idx-1][idy+1])
             # fringe case that the number ends in the bottom corner
@@ -64,13 +66,15 @@ for idx, row in enumerate(arr_data):
                 if num and hit:
                     print(num)
                     num_idx.append(int(num))
-            # another fringe case where a number ends at the end of a line
-            if idx == xmax:
-                if num and hit:
-                    print(num)
-                    num_idx.append(int(num))
                 num = ''
                 hit = False
+            # another fringe case where a number ends at the end of a line
+            # elif idx == xmax:
+            #     if num and hit:
+            #         print(num)
+            #         num_idx.append(int(num))
+            #     num = ''
+            #     hit = False
             
         else:
             if num and hit:
--- day5/day5.py
+++ day5/day5.py
@@ -1,9 +1,10 @@
 import re
+import pprint
 
 cat = {}
 idx = 0
 
-with open('sample') as fp:
+with open('input') as fp:
     for line in fp:
         #print(line.strip())
         if re.search(r"^seeds:", line):
@@ -14,13 +15,29 @@ with open('sample') as fp:
             m = str(re.search(r"(.*)\ map:$", line).groups())
             #print(m)
         elif line.strip() == '':
-            print("empty line")
+            print("index " + str(idx) + " is done!")
             idx = idx + 1
         else:
             data = line.strip().split()
-            if idx in cat:
-                print("present")
-            else:
-                cat[idx] = None
-        
-print(cat)
+            if not idx in cat:
+                cat[idx] = {}
+            for x in range(int(data[2])):
+                cat[idx][int(data[1])+x] = int(data[0]) + x
+
+
+
+pp = pprint.PrettyPrinter(indent=4)
+#pp.pprint(cat)
+
+ans = []
+
+for seed in cat[0]:
+    newseed = int(seed)
+    for z in range(1, len(cat)):
+        if newseed in cat[z]:
+            temp = newseed
+            newseed = cat[z][(temp)]
+    ans.append(newseed)
+
+ans.sort()    
+print("part1: " + str(ans[0]))
